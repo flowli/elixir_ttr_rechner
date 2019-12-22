c434fc5e68bdc92b4901aa8923b7575f3e3deeab
Florian Arndt <post@florianarndt.com>
2019-12-22 16:41:41 +0100

    removed .idea

--- lib/ttr_rechner.ex
+++ lib/ttr_rechner.ex
@@ -6,7 +6,7 @@ defmodule TtrRechner do
   @doc """
   Berechnet deinen neuen TTR-Wert nach einem Spiel.
   """
-  defp new_ttr_score(ttr_score, opponent_ttr_score, won, change_constant = 16) do
+  def new_ttr_score(ttr_score, opponent_ttr_score, won, change_constant = 16) do
     new_ttr_score_if_won = ttr_score + (
       1 - (1 / (1 + :math.pow(10, (opponent_ttr_score - ttr_score) / 150)))) * change_constant
     if won do
