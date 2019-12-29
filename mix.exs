fb0e6e7d36c67206e943091e96bff31b5dd5e43a
Florian Arndt <post@florianarndt.com>
2019-12-29 16:36:38 +0100

    removed .idea

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule TtrRechner.MixProject do
       version: "0.0.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      description: description
     ]
   end
 
@@ -21,6 +23,7 @@ defmodule TtrRechner.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
@@ -36,7 +39,10 @@ defmodule TtrRechner.MixProject do
     [
       files: ["lib", "mix.exs", "README*"],
       maintainers: ["Florian Arndt"],
-      licenses: ["MIT"]
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/flowli/elixir_ttr_rechner"
+      }
     ]
   end
 end
